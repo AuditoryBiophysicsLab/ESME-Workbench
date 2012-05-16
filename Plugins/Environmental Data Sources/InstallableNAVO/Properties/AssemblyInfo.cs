c8eecc8c5824b05fc3da713f39d5784dcc9706c0
Dave Anderson <da@bu.edu>
2012-05-16 01:35:10 +0000

    Installed version now works on test machine

--- Plugins/Environmental Data Sources/InstallableNAVO/Properties/AssemblyInfo.cs
+++ Plugins/Environmental Data Sources/InstallableNAVO/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GDEM3Configuration")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Installable NAVO Plugin")]
+[assembly: AssemblyDescription("Plugin for ESME Workbench to provide environmental data from an installable version of selected, unclassified Naval Oceanographic Office (NAVO) databases")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GDEM3Configuration")]
+[assembly: AssemblyCompany("Boston University")]
+[assembly: AssemblyProduct("ESME Workbench")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
