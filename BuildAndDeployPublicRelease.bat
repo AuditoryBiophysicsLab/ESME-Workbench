284d5f71d11188cfac2c58fb03675a8339212557
Dave Anderson <da@bu.edu>
2012-10-23 23:25:51 +0000

    Updated development and public build scripts to automatically increment the build number, commit everything to the repository, and label the build appropriately.  At this time, the public script has not been tested.

--- BuildAndDeployPublicRelease.bat
+++ BuildAndDeployPublicRelease.bat
@@ -4,11 +4,17 @@ echo to the world.
 echo.
 echo Please be sure you really want to do this!
 echo.
+echo Have you updated the minor version number (if appropriate)?
+echo.
 set /p UserConfirmation="Are you sure? (type 'yes' to confirm) "
 if "%UserConfirmation%"=="yes" goto ProceedWithBuild
 goto DoNotBuild
 :ProceedWithBuild
 echo Starting build
+for /f "usebackq" %%A in (`""C:\Projects\ESME Deliverables\Utilities\VersionInfoIncrementer\bin\Release\VersionInfoIncrementer.exe" "C:\Projects\ESME Deliverables\Solutions\ESME Workbench\VersionInfo.txt""`) do set ESMEVersion=%%A
+svn commit --message "Public release %ESMEVersion%"
+svn update
+svn copy . "https://hrcsvn.bu.edu/svn/esme/src/bu/ESME Deliverables/tags/release builds/%ESMEVersion%" --message "Public release %ESMEVersion%/"
 call "BuildAndDeploy.bat" download
 goto End
 :DoNotBuild
