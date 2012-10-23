fdf2dbabc641f9d3502e8a6206f13352f01b7a51
Dave Anderson <da@bu.edu>
2012-10-23 22:02:04 +0000

    Development release %ESMEVersion

--- BuildAndDeployDevelopmentRelease.bat
+++ BuildAndDeployDevelopmentRelease.bat
@@ -1,1 +1,3 @@
+@for /f "tokens=*" %V in ('Utilities\VersionInfoIncrementer\bin\Debug\VersionInfoIncrementer.exe "C:\Projects\ESME Deliverables\Solutions\ESME Workbench\VersionInfo.txt"') do set ESMEVersion=%V
+
 call "BuildAndDeploy.bat" development
--- Utilities/VersionInfoIncrementer/Program.cs
+++ Utilities/VersionInfoIncrementer/Program.cs
@@ -8,18 +8,25 @@ namespace VersionInfoIncrementer
     {
         static int Main(string[] args)
         {
-            Console.Write("VersionInfoIncrementer command line: ");
-            foreach (var arg in args) Console.Write(arg + " ");
-            Console.WriteLine();
+            //Console.Write("VersionInfoIncrementer command line: ");
+            //foreach (var arg in args) Console.Write(arg + " ");
+            //Console.WriteLine();
             if (args.Length != 1 || !File.Exists(args[0]))
             {
                 Usage();
                 return 1;
             }
             var lines = File.ReadAllLines(args[0]);
+            var foundVersion = false;
             for (var lineIndex = 0; lineIndex < lines.Length; lineIndex++)
             {
                 if (lines[lineIndex].StartsWith("//")) continue;
+                if (foundVersion)
+                {
+                    Console.WriteLine("Error: Multiple version numbers found");
+                    Usage();
+                    return 1;
+                }
                 var curVersion = new Version(lines[lineIndex]);
                 if (curVersion.Major < 0 || curVersion.Minor < 0 || curVersion.Build < 0)
                 {
@@ -29,6 +36,8 @@ namespace VersionInfoIncrementer
                 }
                 var newVersion = new Version(curVersion.Major, curVersion.Minor, curVersion.Build + 1);
                 lines[lineIndex] = newVersion.ToString();
+                Console.Write(newVersion.ToString());
+                foundVersion = true;
             }
             File.WriteAllLines(args[0], lines);
             return 0;
