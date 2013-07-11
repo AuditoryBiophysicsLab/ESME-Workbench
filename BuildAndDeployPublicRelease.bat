7978fc06a067c9d7af8011381c43e2d4de5654fa
Dave Anderson <da@bu.edu>
2013-07-11 02:02:48 +0000

    Development release 3.1.29

--- BuildAndDeployPublicRelease.bat
+++ BuildAndDeployPublicRelease.bat
@@ -12,10 +12,10 @@ goto DoNotBuild
 :ProceedWithBuild
 echo Starting build
 for /f "usebackq" %%A in (`""C:\Projects\ESME Deliverables\Utilities\VersionInfoIncrementer\bin\Release\VersionInfoIncrementer.exe" "C:\Projects\ESME Deliverables\Solutions\ESME Workbench\VersionInfo.txt""`) do set ESMEVersion=%%A
-svn commit --message "Public release %ESMEVersion"
+svn commit --message "Public release %ESMEVersion%"
 svn update
-svn copy . "https://hrcsvn.bu.edu/svn/esme/src/bu/ESME Deliverables/tags/release builds/%ESMEVersion" --message "Public release %ESMEVersion"
-call "BuildAndDeploy.bat" download  %ESMEVersion
+svn copy . "https://hrcsvn.bu.edu/svn/esme/src/bu/ESME Deliverables/tags/release builds/%ESMEVersion" --message "Public release %ESMEVersion%"
+call "BuildAndDeploy.bat" download  %ESMEVersion%
 goto End
 :DoNotBuild
 echo Build canceled
