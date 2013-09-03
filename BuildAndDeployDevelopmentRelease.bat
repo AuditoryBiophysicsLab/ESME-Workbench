76c8236838549eba14bda3d4c72a732b78408e53
Dave Anderson <da@bu.edu>
2013-09-03 16:31:15 -0400

    Development release 3.1.35

--- BuildAndDeployDevelopmentRelease.bat
+++ BuildAndDeployDevelopmentRelease.bat
@@ -5,6 +5,6 @@ rem svn update
 rem svn copy . "https://hrcsvn.bu.edu/svn/esme/src/bu/ESME Deliverables/tags/development builds/%ESMEVersion%" --message "Tag for development release %ESMEVersion%"
 git commit -a -m "Development release %ESMEVersion%"
 git tag -a %ESMEVersion% -m "Development release %ESMEVersion%"
-git push --tags HEAD
+git push --tags
 rem call "BuildAndDeploy.bat" development %ESMEVersion%
 echo Successfully built development release %ESMEVersion%
