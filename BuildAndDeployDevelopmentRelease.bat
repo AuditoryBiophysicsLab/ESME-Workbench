5e6f74bc9b939aa26c8861bd993086014f31b8dd
Dave Anderson <da@bu.edu>
2013-09-03 16:19:31 -0400

    Development release 3.1.35

--- BuildAndDeployDevelopmentRelease.bat
+++ BuildAndDeployDevelopmentRelease.bat
@@ -1,7 +1,10 @@
 @echo off
 for /f "usebackq" %%A in (`""C:\Projects\ESME Deliverables\Utilities\VersionInfoIncrementer\bin\Release\VersionInfoIncrementer.exe" "C:\Projects\ESME Deliverables\Solutions\ESME Workbench\VersionInfo.txt""`) do set ESMEVersion=%%A
-svn commit --message "Development release %ESMEVersion%"
-svn update
-svn copy . "https://hrcsvn.bu.edu/svn/esme/src/bu/ESME Deliverables/tags/development builds/%ESMEVersion%" --message "Tag for development release %ESMEVersion%"
+rem svn commit --message "Development release %ESMEVersion%"
+rem svn update
+rem svn copy . "https://hrcsvn.bu.edu/svn/esme/src/bu/ESME Deliverables/tags/development builds/%ESMEVersion%" --message "Tag for development release %ESMEVersion%"
+git commit -a -m "Development release %ESMEVersion%"
+git tag -a %ESMEVersion% -m "Development release %ESMEVersion%"
+git push
 call "BuildAndDeploy.bat" development %ESMEVersion%
 echo Successfully built development release %ESMEVersion%
