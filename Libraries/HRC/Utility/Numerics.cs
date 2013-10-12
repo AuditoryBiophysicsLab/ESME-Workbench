a8b3ec7633f328a22d2471d98d643d0323d50b99
Dave Anderson <da@bu.edu>
2013-10-12 01:49:37 -0400

    Replacing MathNet.Numerics with ILNumerics for performance reasons.  Still have to replace the last MathNet.Numerics iFFT instance in RamGeo for phased array creation.

--- Libraries/HRC/Utility/Numerics.cs
+++ Libraries/HRC/Utility/Numerics.cs
@@ -2,22 +2,25 @@
 using System.IO;
 using System.Linq;
 using System.Numerics;
-using MathNet.Numerics.IntegralTransforms;
+using ILNumerics;
 
 namespace HRC.Utility
 {
     public static class Numerics
     {
+        static Numerics() { ILMath.FFTImplementation = new ILNumerics.Native.ILFFTW3FFT(); }
+
         public static Complex[] MatlabHilbert(double[] xr)
         {
             //WriteVector(xr, "xr.esme");
             //var fft = new MathNet.Numerics.IntegralTransforms.Algorithms.DiscreteFourierTransform();
-            var x = (from sample in xr select new Complex(sample, 0)).ToArray();
-            Transform.FourierForward(x, FourierOptions.Matlab);
+            //var x = (from sample in xr select new Complex(sample, 0)).ToArray();
+            //Transform.FourierForward(x, FourierOptions.Matlab);
+            var xc = ILMath.fft((ILArray<double>)xr).ToArray();
             //fft.BluesteinForward(x, FourierOptions.Matlab);
             //WriteVector(x, "x_fwd.esme");
-            var h = new double[x.Length];
-            var fftLengthIsOdd = (x.Length | 1) == 1;
+            var h = new double[xc.Length];
+            var fftLengthIsOdd = (xc.Length | 1) == 1;
             if (fftLengthIsOdd)
             {
                 h[0] = 1;
@@ -29,11 +32,22 @@ namespace HRC.Utility
                 h[(xr.Length / 2)] = 1;
                 for (var i = 1; i < xr.Length / 2; i++) h[i] = 2;
             }
-            for (var i = 0; i < x.Length; i++) x[i] *= h[i];
-            Transform.FourierInverse(x, FourierOptions.Matlab);
+            for (var i = 0; i < xc.Length; i++) xc[i] *= h[i];
+            //Transform.FourierInverse(x, FourierOptions.Matlab);
             //fft.BluesteinInverse(x, FourierOptions.Matlab);
             //WriteVector(x, "x_inv.esme");
-            return x;
+            var result = (from value in ILMath.ifft((ILArray<complex>)xc)
+                          select new Complex(value.real, value.imag)).ToArray();
+            return result;
+        }
+
+        public static double[] LeftDivide(double[,] matrix, double[] vector)
+        {
+            if (matrix.GetLength(0) != matrix.GetLength(1)) throw new InvalidOperationException("Cannot calculate the inverse of a non-square matrix");
+            if (matrix.GetLength(0) != vector.GetLength(0)) throw new InvalidOperationException(string.Format("Cannot divide a matrix of size [{0}, {1}] by a vector of size [{2}]", matrix.GetLength(0), matrix.GetLength(1), vector.Length));
+            var input = (ILInArray<double>)matrix;
+            var inverse = ILMath.linsolve(input.T, ILMath.eye(matrix.GetLength(0), matrix.GetLength(1)));
+            return ILMath.multiply(inverse, vector).ToArray();
         }
 
         public static void WriteVector(double[] realvec, string fileName)
